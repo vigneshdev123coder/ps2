05e5ee12c476dd797ef51d445d85bd78e3dfaeae
unknown <vigneshgoud12@gmail.com>
2024-09-12 11:29:29 +0530

    added

---
+++ index.js
@@ -0,0 +1,21 @@
+let a =prompt("enter a number")
+let b =prompt("enter b number")
+let c=prompt("enter c number")
+if(a>b&&a>c){
+    console.log("a is big")
+}
+else if(b>a&&b>c){
+    console.log("b is big")
+}
+else{
+    console.log("c is big")
+}
+
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+
+for(i=10;i>=1;i--){
+    console.log(i)
+}
