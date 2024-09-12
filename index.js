fae860f25628205b84fc39043ccb74e10fec3001
unknown <vigneshgoud12@gmail.com>
2024-09-12 13:06:33 +0530

    added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// finding biggest number amoung three numbers
+
 let a =prompt("enter a number")
 let b =prompt("enter b number")
 let c=prompt("enter c number")
@@ -11,11 +14,14 @@ else{
     console.log("c is big")
 }
 
+
+
+// printing 1-10 numbers
 for(i=1;i<=10;i++){
     console.log(i)
 }
 
-
+//  printing 10-1 numbers
 for(i=10;i>=1;i--){
     console.log(i)
 }
